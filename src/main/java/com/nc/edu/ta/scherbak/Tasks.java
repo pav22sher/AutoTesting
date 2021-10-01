a71bf5d3037e9c34d4f4655ea69bd849e084dad3
Pablo <scherbak.pavel5@yandex.ru>
2021-10-02 01:07:47 +0400

    Incoming test passed!

--- src/main/java/com/nc/edu/ta/scherbak/Tasks.java
+++ src/main/java/com/nc/edu/ta/scherbak/Tasks.java
@@ -1,15 +1,30 @@
 package com.nc.edu.ta.scherbak;
 
-import java.util.Date;
-import java.util.List;
-import java.util.Set;
-import java.util.SortedMap;
+import java.util.*;
 
 public class Tasks {
     public static SortedMap<Date, Set<Task>> timeline(List<Task> tasks, Date from, Date to) {
         return null;
     }
     public static List<Task> incoming(List<Task> tasks, Date from, Date to) {
-        return null;
+        List<Task> result = new ArrayList<>();
+        for(Task task: tasks) {
+            if(task.isActive()) {
+                if (task.isRepeated()) {
+                    for (Date time = task.getStartTime(); time.compareTo(task.getEndTime()) <= 0; time = new Date(time.getTime() + task.getRepeatInterval() * 1000L)) {
+                        if(time.compareTo(from) > 0 && time.compareTo(to) <= 0) {
+                            result.add(task);
+                            break;
+                        }
+                    }
+                } else {
+                    Date time = task.getTime();
+                    if(time.compareTo(from) > 0 && time.compareTo(to) <= 0) {
+                        result.add(task);
+                    }
+                }
+            }
+        }
+        return result;
     }
 }
