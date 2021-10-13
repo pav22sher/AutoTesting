6fe0910fe83a55a5026ed737c68a098ecd7c2671
scherbak <scherbak.pavel5@yandex.ru>
2021-10-13 23:49:35 +0400

    Maven!

---
+++ src/main/java/com/nc/edu/ta/scherbak/TaskXMLSerializer.java
@@ -0,0 +1,17 @@
+package com.nc.edu.ta.scherbak;
+
+public class TaskXMLSerializer {
+    /**
+     * сохранение списка задач в файл
+     */
+    void save(AbstractTaskList object, String file){
+
+    }
+
+    /**
+     * загрузка списка задач из файла file
+     */
+    public AbstractTaskList load(String file) {
+        return null;
+    }
+}
